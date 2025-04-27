77ed2d940c47ee31552744f98eb40eb459d8ab39
Ivyyy294 <48586445+Ivyyy294@users.noreply.github.com>
2025-04-27 20:27:38 +0200

    Update IntroCPP.cpp

--- IntroCPP/IntroCPP.cpp
+++ IntroCPP/IntroCPP.cpp
@@ -3,8 +3,7 @@
 
 #include <iostream>
 #include "MonsterManager.h"
-//#include "romanNumeral.h"
-//#include "Inventory.h"
+
 int main()
 {
 	MonsterManager monsterManager;
@@ -27,25 +26,4 @@ int main()
 	monsterManager.PrintInstruction();
 	monsterManager.ProcessInstruction();
 	monsterManager.DeleteMonsterList();
-	//Inventory inventory;
-	//inventory.Populate();
-	//inventory.OpenInventory();
-	//RomanNumeral romanNumeral1, romanNumeral2, romanNumeralResult;
-	//std::cout << "Give me two roman numerals:" << std::endl;
-	//std::cin >> romanNumeral1 >> romanNumeral2;
-	//romanNumeralResult = romanNumeral1 + romanNumeral2;
-
-	//std::cout << romanNumeral1 << " + " << romanNumeral2
-	//	<< " = " << romanNumeralResult;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
