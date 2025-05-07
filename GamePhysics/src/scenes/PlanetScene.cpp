c2551ce73ef7e2f7cb83f79ea1d7fdf11b625a92
Ivyyy294 <48586445+Ivyyy294@users.noreply.github.com>
2025-05-07 20:58:56 +0200

    🔨 Added sun velocity and planet offset to inspector

--- GamePhysics/src/scenes/PlanetScene.cpp
+++ GamePhysics/src/scenes/PlanetScene.cpp
@@ -24,8 +24,11 @@ void PlanetScene::DrawGUI()
 
 	ImGui::DragFloat("Sun mass", &m_sunMass, 1.0f, 0.001f);
 	ImGui::DragFloat("Sun distance", &m_sunDistance, 0.1f, 0.0f, 10.0f);
-	ImGui::DragFloat("Planet mass", &m_planetMass, 1.0f, 1.0f, 100.0f);
+	ImGui::DragFloat2("Sun velocity", &m_sunVelocity[0], 0.1f);
+
+	ImGui::DragFloat("Planet mass", &m_planetMass, 0.1f, 0.0f, 100.0f);
 	ImGui::DragFloat2("Planet velocity", &m_planetVelocity[0], 0.1f);
+	ImGui::DragFloat2("Planet offset", &m_planetOffset[0], 0.1f);
 	
 	ImGui::Separator();
 
@@ -46,16 +49,16 @@ void PlanetScene::SpawnPlanets()
 {
 	Planet* sun1 = new Planet(0.75f, m_sunMass, ImColor(255, 204, 51));
 	sun1->SetPosition(glm::vec2(-m_sunDistance * 0.5f, 0.0f));
-	sun1->SetVelocity(glm::vec2(0.0f, 4.0f));
+	sun1->SetVelocity(m_sunVelocity);
 	gameObjects.push_back(sun1);
 
 	Planet* sun2 = new Planet(0.75f, m_sunMass, ImColor(255, 204, 51));
 	sun2->SetPosition(glm::vec2(m_sunDistance * 0.5f, 0.0f));
-	sun2->SetVelocity(glm::vec2(0.0f, -4.0f));
+	sun2->SetVelocity(-m_sunVelocity);
 	gameObjects.push_back(sun2);
 
 	Planet* earth = new Planet(0.3, m_planetMass, ImColor(107, 147, 214));
-	earth->SetPosition(sun1->GetPosition() + glm::vec2(-2.0f, 0.0f));
+	earth->SetPosition(sun1->GetPosition() + m_planetOffset);
 	earth->SetVelocity(m_planetVelocity);
 	gameObjects.push_back(earth);
 }
--- GamePhysics/src/scenes/PlanetScene.h
+++ GamePhysics/src/scenes/PlanetScene.h
@@ -12,10 +12,13 @@ public:
 	virtual const char* GetName() override { return "PlanetScene"; };
 	virtual void DrawGUI() override;
 private:
-	float m_sunDistance = 3.3f;
+	float m_sunDistance = 5.0f;
 	float m_sunMass = 105.0f;
-	float m_planetMass = 1.1f;
-	glm::vec2 m_planetVelocity = glm::vec2 (0.0f, -3.0f);
+	glm::vec2 m_sunVelocity = glm::vec2(0.0f, 3.5f);
+
+	float m_planetMass = 0.5f;
+	glm::vec2 m_planetOffset = glm::vec2 (-5.5f, 0.0f);
+	glm::vec2 m_planetVelocity = glm::vec2 (0.0f, -5.5f);
 
 	void Restart();
 	void SpawnPlanets();
