29161b28c5d91423f6971a84a17185ba078dc9ab
Ivyyy294 <48586445+Ivyyy294@users.noreply.github.com>
2025-11-29 15:15:51 +0100

    Update TowerDefense.cs

--- TowerDefence/AI_TowerDefense/TowerDefense.cs
+++ TowerDefence/AI_TowerDefense/TowerDefense.cs
@@ -54,7 +54,7 @@ namespace AI_TowerDefense
             // Your strategy should at least be able to beat random!
             
             _playerAStrategy = new IvyyyStrategy(_playerA);
-            _playerBStrategy = new GongiStratIsolated(_playerB);
+            _playerBStrategy = new RandomStrategy(_playerB);
             //_playerBStrategy = new DummyStrategy(_playerB);
             //_playerBStrategy = new RandomStrategy(_playerB);
             //_playerBStrategy = new LeTudorStrategy(_playerB);
